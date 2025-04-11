7febffb3d3149b4d442ea529e3c783ba4763414b
bartekk202 <bartlomiej.kur07@gmail.com>
2025-04-11 09:32:36 +0200

    wip: simple data structure upgraded

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-users:list = [
-    {"name":"Bartek","location":"Giżycko","posts":400},
-    {"name":"Krzysztof","location":"Białobrzegi","posts":500},
-    {"name":"Maja","location":"Świecie","posts":300},
-    {"name":"Zuzanna","location":"Radzyń_Podlaski","posts":700},
-]
+from utils.model import users
+from utils.controller import get_user_info
+
+def main():
+    get_user_info(users)
 
 
-
-def get_user_info(users_data:list)->None:
-
-
-    for user in users_data:
-        print(f"Twój znajomy {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów")
-
-get_user_info(users)
+if __name__ == "__main__":
+    main()
---
+++ utils/model.py
@@ -0,0 +1,6 @@
+users:list = [
+    {"name":"Bartek","location":"Giżycko","posts":400},
+    {"name":"Krzysztof","location":"Białobrzegi","posts":500},
+    {"name":"Maja","location":"Świecie","posts":300},
+    {"name":"Zuzanna","location":"Radzyń_Podlaski","posts":700},
+]
