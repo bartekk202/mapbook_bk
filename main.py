b69ca797739403fee7358e2d0d9eec2676441a8c
bartekk202 <bartlomiej.kur07@gmail.com>
2025-04-16 10:24:19 +0200

    wip: add menu

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from utils.model import users
 from utils.controller import get_user_info
 
 def main():
-    get_user_info(users)
+    while True:
+
+        print("================MENU=================")
+        print("0 - zakończ program")
+        print("1 - pokaż co u znajomych")
+        print("=====================================")
+        choice = input("wybierz opcje menu: ")
+        if choice == "0": break
+        if choice == "1": get_user_info(users)
+
 
 
 if __name__ == "__main__":
