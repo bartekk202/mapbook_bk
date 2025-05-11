62257bf51fa2b63865399779106debdf193f63ea
bartekk202 <bartlomiej.kur07@gmail.com>
2025-05-11 23:43:13 +0200

    wip: new function: remove user

--- notatnik.py
+++ notatnik.py
@@ -1,21 +1,19 @@
-users: list = [
-    {"name": "Bartek", "location": "Ełk", "posts": 400}
-
+users:list = [
+    {"name":"Zuzia","location":"Radzyń_Podlaski","posts":700},
 
 ]
 print(users)
 
 def remove_user(users_data: list)->None:
 
-    user_name=input("podaj imie uzytkownika do usuniecia: ")
-    for user in users:
+    user_name=input("podaj imię użytkownika do usuniecia: ")
+    for user in users_data:
         if user["name"] == user_name:
             users_data.remove(user)
-
 
 remove_user(users)
 
 
 
-print(users)
 
+print(users)
